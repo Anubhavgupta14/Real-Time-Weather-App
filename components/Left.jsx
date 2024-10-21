71b8e81d338ad91505377a3f28b248c8817a2b70
Anubhav Gupta <anubhavgu2002@gmail.com>
2024-10-21 17:04:28 +0530

    city changes added

--- components/Left.jsx
+++ components/Left.jsx
@@ -10,20 +10,69 @@ import {
   TiWeatherWindyCloudy,
 } from "react-icons/ti"; //sunny, rain
 import Image from "next/image";
-
-const Left = ({ data, formattedDate, weatherCondition, currentTime }) => {
-  
+import { MdArrowBackIos } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
+const Left = ({
+  data,
+  formattedDate,
+  weatherCondition,
+  currentTime,
+  cityIndex,
+  setCityIndex,
+  citiesLenght
+}) => {
   const condition = {
-    0 : {comp:<Image src={"/clear.png"} alt="cover" height={80} width={80} />, svg:<TiWeatherSunny/>},
-    1 : {comp:<Image src={"/Mist.png"} alt="cover" height={80} width={80} />, svg:<TiWeatherWindyCloudy/>},
-    2 : {comp:<Image src={"/Cloudy.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherCloudy/>},
-    3 : {comp:<Image src={"/haze.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherSunny/>},
-    4 : {comp:<Image src={"/rain.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherDownpour/>},
-    5 : {comp:<Image src={"/thunderstorm.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherStormy/>},
-    6 : {comp:<Image src={"/snow.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherSnow/>},
-    7 : {comp:<Image src={"/fog.png"} alt="cover" height={80} width={80} />,svg:<TiWeatherSunny/>},
-  }
-//   console.log(formattedDate);
+    0: {
+      comp: <Image src={"/clear.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherSunny />,
+    },
+    1: {
+      comp: <Image src={"/Mist.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherWindyCloudy />,
+    },
+    2: {
+      comp: <Image src={"/Cloudy.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherCloudy />,
+    },
+    3: {
+      comp: <Image src={"/haze.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherSunny />,
+    },
+    4: {
+      comp: <Image src={"/rain.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherDownpour />,
+    },
+    5: {
+      comp: (
+        <Image src={"/thunderstorm.png"} alt="cover" height={80} width={80} />
+      ),
+      svg: <TiWeatherStormy />,
+    },
+    6: {
+      comp: <Image src={"/snow.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherSnow />,
+    },
+    7: {
+      comp: <Image src={"/fog.png"} alt="cover" height={80} width={80} />,
+      svg: <TiWeatherSunny />,
+    },
+  };
+
+  const handleChangeCity = (dir) => {
+    if(dir){
+      //forward
+      setCityIndex((prev)=> (prev+1)%citiesLenght)
+    }
+    else{
+      if(cityIndex==0){
+        setCityIndex(citiesLenght-1);
+      }
+      else{
+        setCityIndex((prev)=>prev-1);
+      }
+    }
+  };
+  //   console.log(formattedDate);
   return (
     <div className="left">
       <div className="location">
@@ -42,9 +91,12 @@ const Left = ({ data, formattedDate, weatherCondition, currentTime }) => {
         </div>
       </div>
       <div className="temp">
+        <MdArrowBackIos className="arrows" onClick={()=>{handleChangeCity(0)}} />
         <h2 className="main-temp">
-          {data?.temp ?? null}<span className="cel"> o</span>
+          {data?.temp ?? null}
+          <span className="cel"> o</span>
         </h2>
+        <MdArrowForwardIos className="arrows" onClick={()=>{handleChangeCity(1)}}/>
       </div>
       <div className="conditions">
         {condition[weatherCondition]?.svg}
@@ -53,19 +105,22 @@ const Left = ({ data, formattedDate, weatherCondition, currentTime }) => {
       <div className="avgTempdiv">
         <div className="avgCard">
           <p style={{ position: "relative" }}>
-            {(data?.avgTemp ?? null)?.toFixed(2)}<span className="cel3">o</span>
+            {(data?.avgTemp ?? null)?.toFixed(2)}
+            <span className="cel3">o</span>
           </p>
           <p>Average</p>
         </div>
         <div className="avgCard">
           <p style={{ position: "relative" }}>
-            {data?.maxTemp ?? null}<span className="cel3">o</span>
+            {data?.maxTemp ?? null}
+            <span className="cel3">o</span>
           </p>
           <p>Maximum</p>
         </div>
         <div className="avgCard">
           <p style={{ position: "relative" }}>
-            {data?.minTemp ?? null}<span className="cel3">o</span>
+            {data?.minTemp ?? null}
+            <span className="cel3">o</span>
           </p>
           <p>Minimum</p>
         </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react";
 import Left from "../../components/Left";
+import moment from "moment-timezone";
 import Right from "../../components/Right";
 import { getLatestData } from "./api/getDataApi";
 const index = () => {
-  const [data, setData] = useState({})
-  const [isLoading, setIsLoading] = useState(false)
-  const date = new Date(data?.date);
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const date = moment().tz("Asia/Kolkata").startOf("day").toDate();
   const options = { day: "numeric", month: "short" };
   const formattedDate = date.toLocaleDateString("en-IN", options);
-  const [weatherCondition, setWeatherCondition] = useState(data?.main ?? null)
+  const [weatherCondition, setWeatherCondition] = useState(data?.main ?? null);
   const [isNight, setIsNight] = useState(false);
   const [currentTime, setCurrentTime] = useState("");
+  const [cityIndex, setCityIndex] = useState(0);
+  const cities = [
+    "Delhi",
+    "Mumbai",
+    "Chennai",
+    "Bangalore",
+    "Kolkata",
+    "Hyderabad",
+  ];
 
   const checkIsNight = () => {
     const currentTime = new Date();
-    const currentHourIST = currentTime.getUTCHours() + 5 + (currentTime.getUTCMinutes() / 60); // IST time (UTC+5:30)
-    
+    const currentHourIST =
+      currentTime.getUTCHours() + 5 + currentTime.getUTCMinutes() / 60; // IST time (UTC+5:30)
+
     if (currentHourIST >= 19 || currentHourIST < 4) {
       setIsNight(true); // 7 PM to 4 AM
     } else {
@@ -23,38 +34,31 @@ const index = () => {
     }
   };
 
-  useEffect(()=>{
-    if(data?.main=="clear"){
-        setWeatherCondition(0);
-    }
-    else if(data?.main=="Mist"){
-        setWeatherCondition(1)
-    }
-    else if(data?.main=="Clouds"){
-        setWeatherCondition(2)
-    }
-    else if(data?.main=="Haze"){
-        setWeatherCondition(3)
-    }
-    else if(data?.main=="Drizzle" || data?.main=="Rain"){
-        setWeatherCondition(4)
-    }
-    else if(data?.main=="Thunderstorm"){
-        setWeatherCondition(5)
-    }
-    else if(data?.main=="Snow"){
-        setWeatherCondition(6)
-    }
-    else if(data?.main=="Fog"){
-        setWeatherCondition(7)
+  useEffect(() => {
+    if (data?.main == "clear") {
+      setWeatherCondition(0);
+    } else if (data?.main == "Mist") {
+      setWeatherCondition(1);
+    } else if (data?.main == "Clouds") {
+      setWeatherCondition(2);
+    } else if (data?.main == "Haze") {
+      setWeatherCondition(3);
+    } else if (data?.main == "Drizzle" || data?.main == "Rain") {
+      setWeatherCondition(4);
+    } else if (data?.main == "Thunderstorm") {
+      setWeatherCondition(5);
+    } else if (data?.main == "Snow") {
+      setWeatherCondition(6);
+    } else if (data?.main == "Fog") {
+      setWeatherCondition(7);
     }
     checkIsNight();
-  },[data])
+  }, [data]);
 
   const formatRealTime = () => {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
     return `${hours}:${minutes}`;
   };
 
@@ -71,14 +75,13 @@ const index = () => {
 
   const fetchLatestData = async () => {
     try {
-      setIsLoading(true)
-      const data = await getLatestData({ city: "Delhi" });
-      setData(data.data)
+      setIsLoading(true);
+      const data = await getLatestData({ city: cities[cityIndex] });
+      setData(data.data);
     } catch (err) {
       console.log(err);
-    }
-    finally{
-      setIsLoading(false)
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -86,14 +89,23 @@ const index = () => {
     fetchLatestData();
     const intervalId = setInterval(fetchLatestData, 300000);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [cityIndex]);
 
-  if(isLoading) return "Loading..."
+  console.log(cityIndex)
+  if (isLoading) return "Loading...";
   return (
     <div className="parent">
-      <div className={isNight ? "main img":"main"}>
-        <Left data={data} formattedDate={formattedDate} weatherCondition={weatherCondition} currentTime={currentTime}/>
-        <Right data={data}/>
+      <div className={isNight ? "main img" : "main"}>
+        <Left
+          data={data}
+          formattedDate={formattedDate}
+          weatherCondition={weatherCondition}
+          currentTime={currentTime}
+          cityIndex={cityIndex}
+          setCityIndex={setCityIndex}
+          citiesLenght = {cities?.length ?? 0}
+        />
+        <Right data={data} />
       </div>
     </div>
   );
